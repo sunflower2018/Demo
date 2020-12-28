bbe7a05432542cde5b0158d70c9365f66c8fd3e1
sunflower <donglizhog@163.com>
2020-12-28 13:50:54 +0800

    1

--- Demo/Demo.Test/Program.cs
+++ Demo/Demo.Test/Program.cs
@@ -17,7 +17,7 @@ namespace Demo.Test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1()); 
-            //202012281324
+            //202012281350
         }
     }
 }
