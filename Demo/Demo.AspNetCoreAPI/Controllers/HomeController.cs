d0d605e14df0582be7d9ee52018da4f3127b12a4
sunflower <donglizhog@163.com>
2020-12-31 20:53:02 +0800

    1

---
+++ Demo/Demo.AspNetCoreAPI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Demo.AspNetCoreAPI.Token;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.AspNetCoreAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index(string userName, string pwd)
+        {                            
+            return BadRequest(new { message = "username or password is incorrect." });           
+        }
+    }
+}
---
+++ Demo/Demo.AspNetCoreAPI/Token/JWTConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.AspNetCoreAPI.Token
+{
+    public class JWTConfig
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string IssuerSigningKey { get; set; }
+        public int AccessTokenExpiresMinutes { get; set; }
+        public string RefreshTokenAudience { get; set; }
+        public int RefreshTokenExpiresMinutes { get; set; }
+    }
+}
